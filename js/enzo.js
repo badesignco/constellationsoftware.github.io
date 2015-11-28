cff78c47f333a1616ff8a1cb2158e3fb703302b0
badesignco <ben@constellation.software>
2015-11-28 09:31:41 -0500

    Add alpha scroll effect for Hero shot

---
+++ js/enzo.js
@@ -0,0 +1,20 @@
+function fadeHero(percentage) {
+    var hero = document.getElementById("hero");
+    hero.style.opacity="0.5";
+}
+s
+function scrollAlpha() {
+    var object = document.getElementById("hero");
+    var screenPosition = object.getBoundingClientRect();
+    var amountHidden = Math.max(0, screenPosition.bottom - window.innerHeight);
+    var percentHidden = amountHidden / object.height;
+    object.style.opacity = 1 - percentHidden;
+    // var bottom_of_object = object.position().top + object.outerHeight();
+    // var bottom_of_window = window.scrollTop() + window.height();
+    // console.log(bottom_of_window);
+    // if(bottom_of_window > bottom_of_object) {
+    //     object.animate({'opacity': '1'}, 500);
+    // }
+}
+window.onload = fadeHero; 
+window.onscroll = scrollAlpha;
