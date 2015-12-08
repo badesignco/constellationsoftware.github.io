39d0d1383f4b5c0050d9b9e473b32cf5cbfbfbff
badesignco <ben@constellation.software>
2015-12-08 13:04:48 -0500

    Basic Layout Enzo

--- js/enzo.js
+++ js/enzo.js
@@ -1,19 +1,25 @@
-function fadeHero(percentage) {
-    var hero = document.getElementById("hero");
-    hero.style.opacity="0.5";
-}
 
-function scrollAlpha() {
+
+function scrollHero() {
     var object = document.getElementById("hero");
     var screenPosition = object.getBoundingClientRect();
+    console.log(screenPosition.top);
     var amountHidden = Math.max(0, screenPosition.bottom - window.innerHeight);
     var percentHidden = amountHidden / object.height;
-    object.style.opacity = 1 - percentHidden;
+    //object.style.opacity = 1 - percentHidden;
+
+    //object.height = window.innerHeight;
+
+    if (screenPosition.top <= 0) {
+        var tm = screenPosition.top * -1;
+        console.log(tm);
+        object.style.marginTop = tm+"px";
+    }
     // var bottom_of_window = window.scrollTop() + window.height();
     // console.log(bottom_of_window);
     // if(bottom_of_window > bottom_of_object) {
     //     object.animate({'opacity': '1'}, 500);
     // }
 }
-window.onload = fadeHero; 
-window.onscroll = scrollAlpha;
+
+window.onscroll = scrollHero;
